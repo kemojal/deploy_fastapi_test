446b20dbdd63342b6ab899e8ccf5552df1248486
kemo <kemo.jallow@hannlync.com>
2025-05-07 10:25:46 +0800

    push action port error

--- app/main.py
+++ app/main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello from FastAPI!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
